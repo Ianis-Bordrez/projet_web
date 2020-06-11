72e58c82ff4355aefa6aa8bbd42711902f1352f4
Ianis-Bordrez <ianis.bordrez@gmail.com>
2020-06-11 10:58:02 +0200

    fix chat

--- script/s_insert_chat.php
+++ script/s_insert_chat.php
@@ -4,11 +4,13 @@
 
 require_once('config.php');
 
-$req = $db->prepare("INSERT INTO private_message (sender_id, receiver_id, message, status) VALUES (:sender_id, :receiver_id, :message, :status)");
+$rid = htmlspecialchars($_POST['receiver_id']);
+$message = htmlspecialchars($_POST['message']);
+
+$req = $db->prepare("INSERT INTO private_message (sender_id, receiver_id, message, status) VALUES (:sender_id, :receiver_id, :message, 1)");
 $req->bindParam('sender_id', $_SESSION['account_id']);
-$req->bindParam('receiver_id', htmlspecialchars($_POST['receiver_id']));
-$req->bindParam('message', htmlspecialchars($_POST['message']));
-$req->bindParam('status', '1');
+$req->bindParam('receiver_id', $rid);
+$req->bindParam('message', $message);
 $req->execute();
 
 if($req) {
