900f964e8199212cfb112bc159c2b5aafb1b9c7c
laurent.marechal@ynov.com <laurent.marechal@ynov.com>
2020-06-16 15:09:41 +0200

    add wiki

--- script/s_wiki.php
+++ script/s_wiki.php
@@ -1,8 +1,4 @@
 <?php
 var_dump($GLOBALS);
-$req = $db->prepare("SELECT * FROM account WHERE account_id != :account_id ");
-$req->bindParam('account_id', $_SESSION['account_id']);
-$req->execute();
-$results = $req->fetchAll();
-$req->closeCursor();
+require_once('config.php');
 ?>
