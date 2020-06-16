8c094cccc356cda2f0b13aec29c551459f62aae3
Ianis-Bordrez <ianis.bordrez@gmail.com>
2020-06-16 15:39:42 +0200

    search with price

--- wiki.php
+++ wiki.php
@@ -1,25 +1,50 @@
 <?php
 require_once("header.php");
+
+$query = "SELECT * FROM item";
+
+if (isset($_POST["submit"])) {
+    $conditions = array();
+
+    if(!empty($_POST["PriceMin"])) {
+      $conditions[] = "price >= ".$_POST['PriceMin'];
+    }
+    if(!empty($_POST["PriceMax"])) {
+        $conditions[] = "price <= ".$_POST['PriceMax'];
+      }
+
+    if (count($conditions) > 0) {
+      $query .= " WHERE " . implode(' AND ', $conditions);
+    }
+
+}
+
+$req = $db->prepare($query);
+$req->execute();
+$items = $req->fetchAll();
+$req->closeCursor();
+
+
 ?>
 
 <script src="js/wiki.js"></script>
 
 <div class="card darkblue" style="width: 18rem;">
   <div class="card-body">
-    <form action="script/s_wiki.php" method="post" class="form-example">
+    <form action="wiki.php" method="post" class="form-example">
         <h5 class="card-title white">Filtres</h5>
         <p class="card-text white">Prix</p>
         <div class="input-group input-group-sm mb-3">
             <div class="input-group-prepend">
                 <span class="input-group-text" id="inputGroup-sizing-sm">Min</span>
             </div>
-            <input name="PrixMin" type="text" class="form-control" aria-label="Small" aria-describedby="inputGroup-sizing-sm">
+            <input name="PriceMin" type="text" class="form-control" aria-label="Small" aria-describedby="inputGroup-sizing-sm">
         </div>
         <div class="input-group input-group-sm mb-3">
             <div class="input-group-prepend">
                 <span class="input-group-text" id="inputGroup-sizing-sm">Max</span>
             </div>
-            <input name="PrixMax" type="text" class="form-control" aria-label="Small" aria-describedby="inputGroup-sizing-sm">
+            <input name="PriceMax" type="text" class="form-control" aria-label="Small" aria-describedby="inputGroup-sizing-sm">
         </div>
         <p class="card-text white">Niveaux</p>
         <div class="input-group input-group-sm mb-3">
@@ -59,13 +84,26 @@ require_once("header.php");
                 Sura
             </label>
         </div>
-        <button type="submit" class="btn btn-sm">Rechercher</button>
+        <button type="submit" name="submit" lass="btn btn-sm">Rechercher</button>
     </form>
   </div>
 </div>
 
 
 
+<?php
+
+
+foreach($items as $item){
+    $item_name = $item["name"];
+
+    ?>
+    <div class="white"> <?php echo $item_name ?></div>
+
+    <?php
+}
+
+?>
 
 
 
@@ -83,7 +121,6 @@ require_once("header.php");
 
 
 
-
-<?
+<?php
 require_once("footer.php");
 ?>
